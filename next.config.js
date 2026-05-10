8d95e53790a4c64d9543238c28c7d061936a106d
hanyalah <muhammadjakaria8@gmail.com>
2026-05-10 23:45:25 +0700

    kod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/wedding',        // untuk routing
+  assetPrefix: '/wedding/',    // untuk static assets
 }
 
 module.exports = nextConfig
